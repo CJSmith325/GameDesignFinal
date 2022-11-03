8652a9559860417c89f7296d05861f027198352c
jiop89054 <33436645+jiop89054@users.noreply.github.com>
2022-11-02 23:37:36 -0400

    Set up a sample for the presentation
    Made a base for explaining how the game will work at the next checkpoint. The player moves, the light follows the cursor, and it highlights objects in the area that are otherwise unseen.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
 
-    // Update is called once per frame
+    public Camera cam;
+    public Vector2 mousePos;
+
     void Update()
     {
-        
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = movement * speed;
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        rb.rotation = angle;
     }
 }
