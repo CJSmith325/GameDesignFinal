b67c40fc706ce1ed3c452e10ae6f4fc48ce1176f
jiop89054 <33436645+jiop89054@users.noreply.github.com>
2022-11-10 16:02:54 -0500

    Updated sprite properly

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -10,11 +10,14 @@ public class EnemyFollow : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Transform enemyTransform;
+
     private Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        enemyTransform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -25,9 +28,10 @@ public class EnemyFollow : MonoBehaviour
             player = GameObject.Find("PlayerSprite").transform;
             Vector2 direction = player.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
+            //rb.rotation = angle;
             direction.Normalize();
             movement = direction;
+            FlipCorrectly(direction);
         }
     }
 
@@ -43,5 +47,12 @@ public class EnemyFollow : MonoBehaviour
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    private void FlipCorrectly(Vector2 facevector)
+    {
+        if (facevector.x < 0)
+            enemyTransform.localScale = new Vector3(.3f, .3f, enemyTransform.localScale.z);
+        else
+            enemyTransform.localScale = new Vector3(-.3f, .3f, enemyTransform.localScale.z);
     }
 }
