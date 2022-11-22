e80f98bf686fe4b0b2161b9ddce57a98af49a5ff
CJSmith325 <90848885+CJSmith325@users.noreply.github.com>
2022-11-22 13:26:09 -0500

    Invulnerability and it's Indication Added
    Invulnerability where the player flashes red for a 3 second duration and is invulnerable added

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,17 +6,69 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    // couroutine duration timer
+    private float iFrameDuration = 3f;
+    // timer to hard code a stop to enemy attacks, not working at all
+    private float enemyAtkTimer = 3f;
+    // gets sprite renderer of playersprite
+    private SpriteRenderer playerSprite;
+    // comparison variable to detect enemy damage
+    private float playerHealthCompare = 3f;
+    // static float variable for player health
     public static float playerHealth = 3;
+    // text component for health
     public Text currentHealth;
+    // number of times to flash in invulnerabilty coroutine
+    private float numberFlashes = 3f;
+
+    private void Start()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    private IEnumerator Invulnerability ()
+    {
+        Physics2D.IgnoreLayerCollision(4, 2, true);
+        for (int i = 0; i < numberFlashes; i++)
+        {
+            playerSprite.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration / (numberFlashes *2));
+            playerSprite.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numberFlashes * 2));
+
+        }
+        Physics2D.IgnoreLayerCollision(4, 2, true);
+    }
 
     private void Update()
     {
+        if (playerHealthCompare > playerHealth)
+        {
+            StartCoroutine(Invulnerability());
+            playerHealthCompare = playerHealth;
+            
+        }
+
         if (playerHealth <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
         currentHealth.text = playerHealth.ToString();
+
+        enemyAtkTimer -= Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (enemyAtkTimer <= 0)
+        {
+            if (collision.gameObject.CompareTag("Enemy") == true)
+            {
+                playerHealth -= 1;
+                enemyAtkTimer = 3;
+            }
+        }
     }
 }
 
