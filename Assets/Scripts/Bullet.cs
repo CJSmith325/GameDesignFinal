99373b2592f91136ec141c53aedd0499288507c0
jiop89054 <33436645+jiop89054@users.noreply.github.com>
2022-12-05 20:46:55 -0500

    Enemy Particles

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damage;
+    public GameObject particlePrefab;
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            GameObject particleObject = Instantiate(particlePrefab, this.transform.position, this.transform.rotation);
             EnemyHealth targetHealth = collision.gameObject.GetComponent<EnemyHealth>();
             targetHealth.EHealth -= damage;
+
             if (targetHealth.EHealth <= 0)
             {
                 Destroy(collision.gameObject);
